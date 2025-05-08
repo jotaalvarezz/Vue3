7e8a736394d260e759b4bfdd7d367c7c5e782553
jotaalvarezz <jotaalvarezz25@gmail.com>
2025-05-08 15:05:40 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,54 @@
 <template>
   <div>
-    <h1>{{ name.toUpperCase() }}</h1>
+    <h1 class="title">CONTADOR</h1>
     <br />
-    <div class="row" v-for="fruta in arrayFrutas" :key="fruta.id">
-      <div class="col-md-12" @click="selectedFruit(fruta)">
-        <h2 style="text-decoration: underline;">{{ fruta.name }}</h2>
-        <span style="font-weight: bold">{{ fruta.price }}</span>
-        <p>{{ fruta.description }}</p>
+    <div class="card" style="width: 18rem">
+      <div :class="num > 0 ? 'card-body positivo':'card-body negativo'">
+        <h1 class="card-title text-dark text-center">{{ num }}</h1>
+      </div>
+    </div>
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <button class="btn btn-info" @click="contador">Incrementar</button>
+        </div>
+        <div class="col-md-12 mt-2 mb-2">
+          <button class="btn btn-info" @click="restador">Decrementar</button>
+        </div>
+        <div class="col-md-12">
+          <button class="btn btn-info" @click="resetear">Resetear</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const name = "Vue 3";
-const arrayFrutas = [
-  {
-    id: 1,
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-  },
-  {
-    id: 2,
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-  },
-  {
-    id: 3,
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-  },
-];
+const num = ref(0);
 
-const selectedFruit = (item) => console.log(item.name)
+const contador = () => {
+  console.log(num);
+  num.value++;
+};
 
+const restador = () => {
+  num.value--;
+};
+
+const resetear = () => {
+  num.value = 0;
+};
 </script>
+<style scoped>
+.positivo{
+  background-color: green;
+}
+
+.negativo{
+  background-color: red;
+}
+</style>
